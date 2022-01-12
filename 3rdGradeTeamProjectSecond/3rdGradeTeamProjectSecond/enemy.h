f84b2f3bcce95fb0cc1284b4baf15fd09a87e1cc
ShinnosukeGoto <shinmocoga@gmail.com>
2022-01-12 11:43:25 +0900

    検知距離緊急メンテ

--- 3rdGradeTeamProjectSecond/3rdGradeTeamProjectSecond/enemy.h
+++ 3rdGradeTeamProjectSecond/3rdGradeTeamProjectSecond/enemy.h
@@ -244,7 +244,7 @@ public:
     void Update(void);                                                                  // 更新処理
     void Draw(void);                                                                    // 描画処理
     static CEnemy *Create(int type, D3DXVECTOR3 pos, float fStrength = 1.0f,
-        int appearState = APPEAR_STATE_EXIST, float fChargeValue = NOT_EXIST_F, float fSearchDistanceForAppear = 2000.0f);// 生成処理
+        int appearState = APPEAR_STATE_EXIST, float fChargeValue = NOT_EXIST_F, float fSearchDistanceForAppear = 3000.0f);// 生成処理
 
     void DiscoveryTarget(CCharacter *pTarget);                                          // ターゲット発見処理
 
